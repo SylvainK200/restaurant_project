e4bfc67f58c7fe435d93a79a76dcc6ddc717d793
SylvainK200 <kouemosylvain@gmail.com>
2021-03-23 02:48:01 +0100

    singular page part 2, and last videos of second week

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {Dish} from "../shared/dish";
+import { DishService } from '../services/dish-service.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,11 +12,22 @@ import {Dish} from "../shared/dish";
 })
 
 export class DishdetailComponent implements OnInit {
-  @Input()
-  dish ?: Dish;
-  constructor() { }
+  
+  dish : Dish;
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { 
+      this.dish = this.dishservice.getDish("k");
+    }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const id =""+this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
+
+  goBack(): void {
+    this.location.back();
+  }
+
 
 }
