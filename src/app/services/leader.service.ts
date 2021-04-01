2a81ed39d5947bb1f5d12cacb2fb0b59759d78ea
SylvainK200 <kouemosylvain@gmail.com>
2021-04-01 13:08:37 +0100

    commit of promises with reactive js

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {LEADERS} from '../shared/leaders';
 import {Leader} from '../shared/leader';
+import {of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,13 +11,11 @@ export class LeaderService {
   constructor() { }
 
   getLeaders():Promise <Leader[]> {
-    return new Promise(resolve => {setTimeout(()=>resolve(LEADERS),2000) ;
-    });
+    return of(LEADERS).pipe(delay(2000)).toPromise()
+
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve =>{
-      setTimeout(()=>resolve(LEADERS.filter((leader) => leader.featured)[0]),2000)
-    });
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000)).toPromise()
   }
 }
