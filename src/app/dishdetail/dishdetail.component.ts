31a06a6b4dcb01a1d3e8f7ddf8c07fbc43f73a18
SylvainK200 <kouemosylvain@gmail.com>
2021-04-02 20:02:44 +0100

    RXJS part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,7 +4,7 @@ import { DishService } from '../services/dish-service.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+import {switchMap}from 'rxjs/operators';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -14,21 +14,30 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
   
   dish?: Dish;
+  dishIds:string[] = [];
+  prev : string="";
+  next :string = "";
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location) { 
-    this.dishservice.getDish("k").then(dish=>this.dish=dish)
+    this.dishservice.getDish("k").subscribe(dish=>this.dish=dish)
       
     }
 
   ngOnInit() {
-    const id =""+this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).then(dish=>this.dish=dish);
+    this.dishservice.getDishId().subscribe(dishIds=>this.dishIds=dishIds);
+
+    this.route.params.pipe  (switchMap((params:Params)=>this.dishservice.getDish(params['id'])))
+    .subscribe(dish=>{this.dish=dish;this.setPrevNext(dish.id)})
   }
 
   goBack(): void {
     this.location.back();
   }
-
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
 
 }
--- src/app/services/dish-service.service.ts
+++ src/app/services/dish-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Dish} from "../shared/dish";
 import {DISHES} from "../shared/dishes";
-import {of} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {delay} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
@@ -9,16 +9,20 @@ import {delay} from 'rxjs/operators';
 export class DishService {
 
   constructor() { }
-  getDishes():Promise<Dish[]>{
-    return of(DISHES).pipe(delay(2000)).toPromise();
+  getDishes():Observable<Dish[]>{
+    return of(DISHES).pipe(delay(2000));
   }
 
-  getDish(id: string): Promise<Dish> {
-    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise()
+  getDish(id: string):Observable<Dish> {
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedDish(): Promise<Dish> {
-    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)).toPromise()
+  getFeaturedDish(): Observable<Dish> {
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+  }
+
+  getDishId():Observable<String[]|any>{
+    return of(DISHES.map(dish=>dish.id));
   }
 }
  
